276c2e12d0bbe8a689cf13ea7ce1a8ebe6bf8ab7
Larens94 <fabrizio.corpora@gmail.com>
2023-11-10 15:59:08 +0100

    update setup

--- setup.py
+++ setup.py
@@ -51,7 +51,8 @@ news-please is an open source, easy-to-use news crawler that extracts structured
           'hurry.filesize>=0.9',
           'bs4',
           'faust-cchardet>=2.1.18',
-          'boto3'
+          'boto3',
+          'scrapy-proxymesh-py3'
       ],
       extras_require={
           ':sys_platform == "win32"': [
