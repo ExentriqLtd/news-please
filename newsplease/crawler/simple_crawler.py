458e68b7cc70e92380b308eb3d2f78996be0ee59
Larens94 <fabrizio.corpora@gmail.com>
2023-11-15 16:56:37 +0100

    feat(proxy_rotation) add proxy rotation by env variables

--- newsplease/crawler/simple_crawler.py
+++ newsplease/crawler/simple_crawler.py
@@ -20,6 +20,38 @@ HEADERS = {
 }
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+def get_proxy_rotation(proxys: dict, http_port: str, https_port: str, username: str, password:str):
+    """
+    Obtains the configuration for proxy rotation.
+
+    Args:
+    - proxys (dict): IPs address or URL of the proxy.
+    - http_port (str): Port for HTTP connections.
+    - https_port (str): Port for HTTPS connections.
+    - username (str): Username for proxy authentication.
+    - password (str): Password for proxy authentication.
+
+    Returns:
+    - dict: A dictionary containing details of the proxy configuration and HTTP/HTTPS ports,
+            along with authentication credentials (username and password).
+    """
+    formatted_proxies = []
+    try:
+        proxies = proxy.split(', ')  
+        formatted_proxies = []
+        for proxy in proxies:
+            formatted_proxy_http = f"http://{username}:{password}@{proxy}:{http_port}"
+            formatted_proxy_https = f"https://{username}:{password}@{proxy}:{https_port}"
+            obj = {
+                "http": formatted_proxy_http,
+                "https": formatted_proxy_https,
+            }
+            formatted_proxies.append(obj)
+    except Exception as ex:
+        LOGGER.error(ex)
+        
+    return formatted_proxies
+
 
 class SimpleCrawler(object):
     _results = {}
@@ -45,14 +77,19 @@ class SimpleCrawler(object):
         """
         html_str = None
         # send
-        PROXY_HTTP = os.getenv('PROXYMESH_HTTP_URL')
-        PROXY_HTTPS = os.getenv('PROXYMESH_HTTPS_URL')
+        PROXYS = os.getenv('PROXYS_MESH')
+        PROXY_USERNAME = os.getenv('PROXY_USERNAME')
+        PROXY_PASSWORD = os.getenv('PROXY_PASSWORD')
+        PROXY_HTTP_PORT = os.getenv('PROXY_HTTP_PORT')
+        PROXY_HTTPS_PORT = os.getenv('PROXY_HTTPS_PORT')
+        # Check if all variables exist
+        required_variables = [PROXYS, PROXY_USERNAME, PROXY_PASSWORD, PROXY_HTTP_PORT, PROXY_HTTPS_PORT]
+        all_variables_exist = all(variable is not None for variable in required_variables)
         proxys = None
-        if PROXY_HTTP is not None and PROXY_HTTPS is not None:
-            proxys = {
-                'http': PROXY_HTTP,
-                'https': PROXY_HTTPS
-            }
+        if all_variables_exist:
+            proxys = get_proxy_rotation(proxys=PROXYS, http_port=PROXY_HTTP_PORT, https_port=PROXY_HTTPS_PORT, username=PROXY_USERNAME, password=PROXY_PASSWORD)
+        else:
+            LOGGER.info('start withount proxymesh: Some or all of the required environment variables are missing.')
         try:
             # read by streaming chunks (stream=True, iter_content=xx)
             # so we can stop downloading as soon as MAX_FILE_SIZE is reached
